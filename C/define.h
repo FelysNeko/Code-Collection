1d7cbb0c34ce4493be7d0b199a5691604591fcd2
Jhanny-Kin <88437453+Jhanny-Kin@users.noreply.github.com>
2023-12-11 22:54:01 -0500

    Create define.h

---
+++ C/define.h
@@ -0,0 +1,13 @@
+#define CALLOC(size, type) (type *)calloc(size, sizeof(type)); ++counter;
+#define FREE(ptr) if (ptr!=NULL) {free(ptr); --counter;}
+
+#define RED(msg) "\033[1;31m"msg"\033[0m\n"
+#define GREEN(msg) "\033[1;32m"msg"\033[0m\n"
+#define YELLOW(msg) "\033[1;33m"msg"\033[0m\n"
+#define BLUE(msg) "\033[1;34m"msg"\033[0m\n"
+#define MAGENTA(msg) "\033[1;35m"msg"\033[0m\n"
+#define CYAN(msg) "\033[1;36m"msg"\033[0m\n"
+#define WHITE(msg) "\033[1;37m"msg"\033[0m\n"
+
+
+extern unsigned long counter;
